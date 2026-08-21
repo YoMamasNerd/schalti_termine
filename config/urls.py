9c9298c2927f9eb6e97f32e1187a4061e5d003be
Jonas Eisele <jonnyboy2212@gmail.com>
2026-08-21 21:07:04 +0200

    fix: restrict Stammdaten navigation and Django Admin strictly to superusers

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,9 @@ from django.urls import include, path
 admin.site.site_header = "Schalti Termine – Verwaltung"
 admin.site.site_title = "Schalti Termine"
 admin.site.index_title = "Verwaltung"
+admin.site.has_permission = lambda request: bool(
+    request.user and request.user.is_active and request.user.is_superuser
+)
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
